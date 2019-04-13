d48552ef2dc3a3053f14019c3eec778dd271f047
vizhnu <vishnushaji231@gamil.com>
2019-04-13 21:52:01 +0530

    add blockchain test

--- blockchain.js
+++ blockchain.js
@@ -1,15 +1,17 @@
-const block = require('./block');
+const Block = require('./block');
 
-class BlockChain{
+class Blockchain{
     constructor(){
         this.chain = [Block.genesis()];
     }
 
     addBlock(data){
-        let lastBlock = this.chain[-1];
+        let lastBlock = this.chain[this.chain.length-1];
         const block = Block.mineBlock(lastBlock, data);
         this.chain.push(block);
 
         return block;
     }
 }
+
+module.exports = Blockchain;
